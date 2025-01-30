44cfba44c7855f1b17fa35c74f32d99704bdab1d
StudentSkunek <281070@student.pwr.edu.pl>
2025-01-30 11:16:23 +0000

    poprawa

--- zadania/zadnie.py
+++ zadania/zadnie.py
@@ -1,8 +1,9 @@
 # Tu trzeba dodać funkcję która zrobi z liczby podanej jako argument liczę o jedne większą i ją zwróci!
 def funkcaj(n):
    return n + 1
+if __name__== "__main__":
+    print(funkcaj(4))
 
-print(funkcaj(4))
 
 # No nie działa tak jak powinno.
 # Importuję funckę a ona się wykonuje
